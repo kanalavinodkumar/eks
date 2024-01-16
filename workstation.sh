57cde190e851ace4a3f6f9838508109ff5ef28f2
kanalavinodkumar <kanalavinodkumar@gmail.com>
2024-01-16 10:06:47 +0530

    updated workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -46,7 +46,7 @@ VALIDATE $? "Downloaded docker-compose"
 chmod +x /usr/local/bin/docker-compose
 VALIDATE $? "Moved docker-compose to local bin"
 
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.1/2023-04-19/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.1/2023-04-19/bin/linux/amd64/kubectl &>>$LOG
 chmod +x ./kubectl &>>$LOG
 mv ./kubectl /usr/local/bin/
 VALIDATE $? "kubectl Installation"
